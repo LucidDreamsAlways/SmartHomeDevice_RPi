2e4760bddf2443616d1c4ec328c1e44bf1602638
Nagul <nagulxlugan@gmail.com>
2023-01-28 18:02:45 +0800

    update

--- Laptop/writesmarthomeDB.py
+++ Laptop/writesmarthomeDB.py
@@ -1,12 +1,10 @@
-import json
 import paho.mqtt.client as mqtt
 from influxdb import InfluxDBClient
-import time
 
 # MQTT connection details
 MQTT_BROKER = "172.20.10.12"
 MQTT_PORT = 1883
-MQTT_TOPICS = ["smarthome/BME280/summary", "smarthome/BME280/temperature", "smarthome/BME280/humidity", "smarthome/BME280/pressure"]
+MQTT_TOPICS = ["smarthome/BME280/temperature", "smarthome/BME280/humidity", "smarthome/BME280/pressure", "smarthome/fan/status"]
 
 # InfluxDB connection details
 INFLUXDB_HOST = "localhost"
@@ -29,20 +27,20 @@ def on_connect(client, userdata, flags, rc):
 # MQTT callback for when a message is received on one of the subscribed topics
 def on_message(client, userdata, msg):
     print("Received message on topic " + msg.topic + ": " + str(msg.payload))
-
-    # Parse incoming JSON data
-    data = json.loads(msg.payload)
+    payload = msg.payload.decode("utf-8") # decode the payload from bytes to string
     measurement = msg.topic.split("/")[-1]
-
-    # Create InfluxDB data point
+    try:
+        # Try to convert the payload to a float
+        value = float(payload)
+    except ValueError:
+        # If it fails, assume the payload is a string
+        value = payload
     data_point = [
         {
             "measurement": measurement,
-            "fields": data
+            "fields": {"value": value}
         }
     ]
-
-    # Write data point to InfluxDB
     dbclient.write_points(data_point)
 
 # Initialize MQTT client
@@ -52,11 +50,5 @@ client.on_message = on_message
 client.connect(MQTT_BROKER, MQTT_PORT, 60)
 
 # Main loop
-while True:
-    client.loop()
-    time.sleep(2)
-
-
-
-
+client.loop_forever()
 
