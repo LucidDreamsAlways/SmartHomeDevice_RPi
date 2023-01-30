cf291c3e55e61cdb9beeefc4a6e0bc0332a8a6bb
Nagul <nagulxlugan@gmail.com>
2023-01-30 10:25:15 +0800

    updating the fan from mqtt
    testing to see whats the problem

---
+++ Laptop/Main_subscriber copy.py
@@ -0,0 +1,30 @@
+import paho.mqtt.client as mqtt
+
+# List of topics to subscribe to
+#topics = ["smarthome/BME280/summary", "smarthome/BME280/temperature", "smarthome/BME280/humidity", "smarthome/BME280/pressure", "smarthome/fan/control", "smarthome/fan/status"]
+
+#I belive if you do "0" it will subscribe to all available topics
+#topics = ["0"]
+
+topics = ["smarthome/fan/control", "smarthome/fan/status"]
+
+# Callback function for when a message is received on a subscribed topic
+def on_message(client, userdata, msg):
+    print("Received message on topic: ", msg.topic)
+    print("Message: ", msg.payload)
+
+# Create an MQTT client
+client = mqtt.Client()
+
+# Set the callback function for when a message is received
+client.on_message = on_message
+
+# Connect to the MQTT broker
+client.connect("172.20.10.12", 1883, 60)
+
+# Subscribe to the topics in the list
+for topic in topics:
+    client.subscribe(topic)
+
+# Start the client loop
+client.loop_forever()
--- Unused:backUP files/Main_subscriber copy.py
+++ Unused:backUP files/Main_subscriber copy.py
@@ -1,3 +1,6 @@
+Down below are codes from two different files, i am not able to control the fan from the mqtt broker. can you find the error?
+
+#code for SensorData.py
 import bme280
 import smbus2
 import time
@@ -84,5 +87,60 @@ while True:
     
     # Delay for 2 seconds
     time.sleep(2)
-    client.loop()
+
+
+#code for Testsmarthomedb.py
+import paho.mqtt.client as mqtt
+from influxdb import InfluxDBClient
+
+# MQTT connection details
+MQTT_BROKER = "172.20.10.12"
+MQTT_PORT = 1883
+MQTT_TOPICS = ["smarthome/BME280/temperature", "smarthome/BME280/humidity", "smarthome/BME280/pressure", "smarthome/fan/status", "smarthome/fan/control"]
+
+# InfluxDB connection details
+INFLUXDB_HOST = "localhost"
+INFLUXDB_PORT = 8086
+INFLUXDB_USER = "root"
+INFLUXDB_PASSWORD = "root"
+INFLUXDB_DBNAME = "smarthome_db"
+
+# Initialize InfluxDB client
+dbclient = InfluxDBClient(INFLUXDB_HOST, INFLUXDB_PORT, INFLUXDB_USER, INFLUXDB_PASSWORD, INFLUXDB_DBNAME)
+
+# MQTT callback for when the client receives a CONNACK response from the server
+def on_connect(client, userdata, flags, rc):
+    print("Connected to MQTT broker with result code " + str(rc))
+
+    # Subscribe to all topics in MQTT_TOPICS
+    for topic in MQTT_TOPICS:
+        client.subscribe(topic)
+
+# MQTT callback for when a message is received on one of the subscribed topics
+def on_message(client, userdata, msg):
+    print("Received message on topic " + msg.topic + ": " + str(msg.payload))
+    payload = msg.payload.decode("utf-8") # decode the payload from bytes to string
+    measurement = msg.topic.split("/")[-1]
+    try:
+        # Try to convert the payload to a float
+        value = float(payload)
+    except ValueError:
+        # If it fails, assume the payload is a string
+        value = payload
+    data_point = [
+        {
+            "measurement": measurement,
+            "fields": {"value": value}
+        }
+    ]
+    dbclient.write_points(data_point)
+
+# Initialize MQTT client
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+client.connect(MQTT_BROKER, MQTT_PORT, 60)
+
+# Main loop
+client.loop_forever()
 
